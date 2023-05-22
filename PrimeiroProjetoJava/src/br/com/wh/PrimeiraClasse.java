22a4de010b68b3eed7e3bba1820fb4235a430cd4
WanderHenrique <wander.h@hotmail.com>
2023-05-22 19:09:53 -0300

    adicionando arquivos ao repositório

---
+++ PrimeiroProjetoJava/src/br/com/wh/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+package br.com.wh;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		System.out.print("HelloWorld");
+	}
+
+}
---
+++ PrimeiroProjetoJava/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author wande
+ *
+ */
+module PrimeiroProjetoJava {
+}
